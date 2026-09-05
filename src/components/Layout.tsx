2d68e22de09fd10dcacc464ecc273c2980c7cf28
Vitor Santos <128942186+vihisantos@users.noreply.github.com>
2026-09-04 23:40:23 -0300

    feat: adiciona camada de experiencia global

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,12 +2,14 @@ import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
 import CartDrawer from './CartDrawer';
+import ExperienceLayer from './ExperienceLayer';
 
 export default function Layout() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col relative">
+      <ExperienceLayer />
       <Header />
-      <main className="flex-1 pt-20">
+      <main className="flex-1 pt-20 relative z-10">
         <Outlet />
       </main>
       <Footer />
